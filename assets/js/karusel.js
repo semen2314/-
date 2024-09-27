63fb7a71476d7a92b6e12358f39afceedec86fc4
semen2314 <83389605+semen2314@users.noreply.github.com>
2024-09-27 22:49:59 +0300

    12
    тест карусели

---
+++ assets/js/karusel.js
@@ -0,0 +1,17 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.achievement-image');
+const totalImages = images.length;
+
+// Устанавливаем первое изображение как активное
+images[currentIndex].classList.add('active');
+
+function showNextImage() {
+    images[currentIndex].classList.remove('active'); // Скрыть текущее изображение
+    currentIndex = (currentIndex + 1) % totalImages; // Перейти к следующему изображению
+    images[currentIndex].classList.add('active'); // Показать следующее изображение
+}
+
+// Меняем изображение каждые 3 секунды
+setInterval(showNextImage, 3000);
+
+
