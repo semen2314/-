43138792633c76d8761e9f8cc31551fe65496288
Снежок <139562158+Kroshkos@users.noreply.github.com>
2024-09-27 17:47:56 +0300

    5
    Добавлены перевертыши

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Spectral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function($) {
 
@@ -79,5 +74,21 @@
 			});
 
 		}
+		var countSquare = $('.square').length;
+
+  		//For each Square found add BG image
+  		for (i = 0; i < countSquare; i++) {
+    		var firstImage = $('.square').eq([i]);
+    		var secondImage = $('.square2').eq([i]);
+
+    	var getImage = firstImage.attr('data-image');
+    	var getImage2 = secondImage.attr('data-image');
+
+    	firstImage.css('background-image', 'url(' + getImage + ')');
+    	secondImage.css('background-image', 'url(' + getImage2 + ')');
+  }
+
+
+
 
 })(jQuery);
